46e7b5fb68006b2fcb21435c9208e6bed48e8ea5
Laec Lekander <laec@ecolibrium3.org>
2024-02-29 11:25:44 -0600

    Set up structure, fixed nav menu

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function expandAndHighlight(category_id,subcategory_id,category_collapse_id) {
+	var category = document.getElementById(category_id)
+	var subcategory = document.getElementById(subcategory_id)
+	var category_collapse = document.getElementById(category_collapse_id)
+
+	category.classList.add('highlight')
+
+	subcategory.classList.add('highlight')
+
+	$('#' + category_collapse_id).attr("style","transition: none !important;")
+	$('#' + category_collapse_id).collapse({
+		show: true
+	})
+	$('#' + category_collapse_id).attr("style","")
+}

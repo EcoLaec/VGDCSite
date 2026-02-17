bc88f3510a847cd3a31fcbaba96491ec6073ecda
EcoLaec <157534603+EcoLaec@users.noreply.github.com>
2026-02-17 10:54:08 -0600

    Adding newer version

---
+++ 2026/js/script.js
@@ -0,0 +1,49 @@
+function expandAndHighlight(category_id,subcategory_id,category_collapse_id) {
+	var category = document.getElementById(category_id)
+	var subcategory = document.getElementById(subcategory_id)
+	var category_collapse = document.getElementById(category_collapse_id)
+
+	category.classList.add('highlight')
+
+	subcategory.classList.add('highlight')
+
+	$('#' + category_collapse_id).attr("style","transition: none !important;")
+	$('#' + category_collapse_id).collapse({
+		show: true
+	})
+	$('#' + category_collapse_id).attr("style","")
+}
+
+function applyComplexity() {
+	$('.complexity').each(function() {
+		var val = parseFloat($(this).html());
+		$(this).html('<i class="bi bi-star-fill"></i>');
+		val -= 1.0;
+		for (let i = 0; i <= 1; i++) {
+			if (val >= 1.0) {
+				$(this).append('<i class="bi bi-star-fill"></i>');
+				val -= 1.0;
+			} else if (val >= 0.5) {
+				$(this).append('<i class="bi bi-star-half"></i>');
+				val -= 0.5;
+			} else {
+				$(this).append('<i class="bi bi-star"></i>');
+			}
+		}
+	});
+}
+
+function toggleSideBar() {
+	$(".bg-dark:first-of-type").toggleClass("t-none");
+}
+
+function copyCode(btn) {
+	var pre = $(btn).parent().children('pre')[0];
+	var code = $(pre).children('code')[0];
+	var text = $(code).text();
+	var tempArea = $('<textarea>')
+	$('body').append(tempArea);
+	tempArea.val(text).select();
+	document.execCommand("copy");
+	tempArea.remove();
+}
